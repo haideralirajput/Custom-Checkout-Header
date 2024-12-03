cbd1b26970d64aadc21ab13b08dbe80640f0d42a
Muhommad Haider Ali <nawazhadi92@gmail.com>
2024-12-03 00:21:27 -0800

    Initial commit

---
+++ extensions/checkout-header-extension/src/Extension.jsx
@@ -0,0 +1,76 @@
+import {
+    Divider,
+    InlineLayout,
+    Link,
+    Text,
+    View,
+    useBuyerJourneySteps,
+    useBuyerJourneyActiveStep,
+    useShop,
+    useTranslate,
+  } from '@shopify/ui-extensions-react/checkout';
+  
+  export default function Extension() {
+    const steps = useBuyerJourneySteps();
+    const activeStep = useBuyerJourneyActiveStep();
+    const {storefrontUrl} = useShop();
+    const translate = useTranslate();
+  
+    const assembledSteps = [
+      {label: translate('cart'), handle: 'cart', to: new URL('/cart', storefrontUrl).href},
+      ...steps,
+    ];
+  
+    const activeStepIndex = assembledSteps.findIndex(
+      ({handle}) => handle === activeStep?.handle,
+    );
+  
+    return (
+      <InlineLayout
+        accessibilityRole="orderedList"
+        spacing="none"
+        maxInlineSize="fill"
+        border="base"
+        padding="none"
+        cornerRadius="base">
+        {assembledSteps.map(({label, handle, to}, index) => (
+          <InlineLayout
+            accessibilityRole="listItem"
+            inlineAlignment="end"
+            blockAlignment="center"
+            key={handle}
+            spacing="none"
+            columns={['fill', 'auto']}>
+            <View
+              inlineAlignment="center"
+              minInlineSize="100%"
+              padding="extraTight"
+              cornerRadius={
+                index === assembledSteps.length - 1
+                  ? ['none', 'base', 'base', 'none']
+                  : 'none'
+              }
+              background={
+                activeStep?.handle === handle ? 'subdued' : 'transparent'
+              }>
+              {index < activeStepIndex || handle === 'cart' ? (
+                <Link to={to}>{label}</Link>
+              ) : (
+                <Text
+                  emphasis={activeStep?.handle === handle ? 'bold' : undefined}
+                  appearance={
+                    activeStep?.handle !== handle ? 'subdued' : undefined
+                  }>
+                  {label}
+                </Text>
+              )}
+            </View>
+            {index < assembledSteps.length - 1 ? (
+              <Divider direction="block" />
+            ) : null}
+          </InlineLayout>
+        ))}
+      </InlineLayout>
+    );
+  }
+  
